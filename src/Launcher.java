8aa7f9bab6689c301b1b265e54cb6ff4a09efe86
vitalijbukatkin <vitaliy.bukatkin@gmail.com>
2019-02-01 15:09:56 +0300

    show login

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,15 +1,16 @@
+import controller.Login;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import util.HibernateUtil;
 
 public class Launcher extends Application {
     public static void main(String[] args){
-        HibernateUtil.getSessionFactory();
         launch();
         HibernateUtil.shutdown();
     }
 
     @Override
     public void start(Stage primaryStage){
+        Login.show();
     }
 }
